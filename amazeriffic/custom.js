785490cd0c4073058c767b5862bc039b293daa14
Joel Licklider <ghoal3@gmail.com>
2017-11-28 12:37:17 +0900

    changed file

--- amazeriffic/custom.js
+++ amazeriffic/custom.js
@@ -1,6 +1,29 @@
-var main = function() {
+var main = function () {
     "use strict";
-    console.log("hello world!");
+    $(".tabs a:nth-child(1)").on("click", function () {
+        $(".tabs span").removeClass("active");
+
+        $(".tabs a:nth-child(1) span").addClass("active");
+
+        $("main .content").empty();
+
+        return false;
+
+    });
+
+    $(".tabs a:nth-child(2)").on("click", function () {
+        $(".tabs span").removeClass("active");
+        $(".tabs a:nth-child(2) span").addClass("active");
+        $("main .content").empty();
+        return false;
+    });
+
+    $(".tabs a:nth-child(3)").on("click", function () {
+        $(".tabs span").removeClass("active");
+        $(".tabs a:nth-child(3) span").addClass("active");
+        $("main .content").empty();
+        return false;
+    });
 };
 
 $(document).ready(main);
