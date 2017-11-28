28d257e662ded6716d1190a59cec1ea4a8b9f182
Joel Licklider <ghoal3@gmail.com>
2017-11-28 12:56:30 +0900

    changed file

--- amazeriffic/custom.js
+++ amazeriffic/custom.js
@@ -1,29 +1,13 @@
 var main = function () {
     "use strict";
-    $(".tabs a:nth-child(1)").on("click", function () {
-        $(".tabs span").removeClass("active");
 
-        $(".tabs a:nth-child(1) span").addClass("active");
-
-        $("main .content").empty();
-
-        return false;
-
-    });
-
-    $(".tabs a:nth-child(2)").on("click", function () {
-        $(".tabs span").removeClass("active");
-        $(".tabs a:nth-child(2) span").addClass("active");
-        $("main .content").empty();
-        return false;
-    });
-
-    $(".tabs a:nth-child(3)").on("click", function () {
-        $(".tabs span").removeClass("active");
-        $(".tabs a:nth-child(3) span").addClass("active");
-        $("main .content").empty();
-        return false;
+   $(".tabs span").toArray().forEach(function (element) {
+       // create a click handler for this element
+       $(element).on("click", function () {
+           $(".tabs span").removeClass("active");
+           $(element).addClass("active");
+           $("main .content").empty();
+           return false;
+       });
     });
 };
-
-$(document).ready(main);
