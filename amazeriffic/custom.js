6865f0106cd84cf8e10676ac04bd63906868be6a
Joel Licklider <ghoal3@gmail.com>
2017-11-28 13:03:53 +0900

    changed file

--- amazeriffic/custom.js
+++ amazeriffic/custom.js
@@ -4,10 +4,20 @@ var main = function () {
    $(".tabs span").toArray().forEach(function (element) {
        // create a click handler for this element
        $(element).on("click", function () {
-           $(".tabs span").removeClass("active");
-           $(element).addClass("active");
-           $("main .content").empty();
-           return false;
+            var $element = $(element);
+
+            $(".tabs a span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
+
+            if ($element.parent().is(":nth-child(1)")) {
+                console.log(" First tab clicked");
+            } else if ($element.parent().is(":nth-child(2)")) {
+                console.log(" Second tab clicked");
+            } else if ($element.parent().is(":nth-child(3)")) {
+                console.log(" Third tab clicked");
+            }
+            return false;
        });
     });
 };
